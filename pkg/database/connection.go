6659dd905121a6341b60611978b05904a674c268
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2024-07-02 08:26:34 -0300

    feat: implement database package

---
+++ pkg/database/connection.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+func NewConnection() (*sql.DB, error) {
+	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
